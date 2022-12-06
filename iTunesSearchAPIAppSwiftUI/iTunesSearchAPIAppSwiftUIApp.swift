9cc7ed9fc3263a4b6c67c6830d6e74883d1cb721
RinStar2003 <mituha133@gmail.com>
2022-12-06 16:26:51 +0200

    Initial Commit

---
+++ iTunesSearchAPIAppSwiftUI/iTunesSearchAPIAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesSearchAPIAppSwiftUIApp.swift
+//  iTunesSearchAPIAppSwiftUI
+//
+//  Created by мас on 06.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct iTunesSearchAPIAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
